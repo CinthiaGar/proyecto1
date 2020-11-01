97bb8aefe199e3559d5f2d8834adb64be3c02e81
Manuel Cabezas <manuelcabezas@gmail.com>
2020-11-01 19:47:30 -0300

    separación de javascript a un archivo

---
+++ js/graficos.js
@@ -0,0 +1,46 @@
+var $colTotal = document.getElementById('chartColesterolTotal').getContext('2d');
+var $trigliceridos = document.getElementById('chartTrigliceridos').getContext('2d');
+
+var chartColesterolTotal = new Chart($colTotal, {
+    type: 'line',
+    data: {
+        labels: ['15/06/15', '03/03/16', '23/09/16', '10/01/17', '25/03/18', '20/05/19', '01/04/20'],
+        datasets: [{
+            label: 'Colesterol Total',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [242, 305, 280, 205, 200, 190, 201]
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+var chartTrigliceridos = new Chart($trigliceridos, {
+    type: 'bar',
+    data: {
+        labels: ['15/06/15', '03/03/16', '23/09/16', '10/01/17', '25/03/18', '20/05/19', '01/04/20'],
+        datasets: [{
+            label: 'Trigliceridos',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [154, 250, 190, 160, 120, 130, 150]
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
