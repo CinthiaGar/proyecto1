aa8415a4ecf9b68fdcd1274782b8ac7c4cb5f954
Manuel Cabezas <manuelcabezas@gmail.com>
2020-11-01 21:19:08 -0300

    cambio de gráficos

--- js/graficos.js
+++ js/graficos.js
@@ -7,8 +7,8 @@ var chartColesterolTotal = new Chart($colTotal, {
         labels: ['15/06/15', '03/03/16', '23/09/16', '10/01/17', '25/03/18', '20/05/19', '01/04/20'],
         datasets: [{
             label: 'Colesterol Total',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgb(175, 171, 171)',
+            borderColor: 'rgb(175, 171, 171)',
             data: [242, 305, 280, 205, 200, 190, 201]
         }]
     },
@@ -29,8 +29,8 @@ var chartTrigliceridos = new Chart($trigliceridos, {
         labels: ['15/06/15', '03/03/16', '23/09/16', '10/01/17', '25/03/18', '20/05/19', '01/04/20'],
         datasets: [{
             label: 'Trigliceridos',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgb(175, 171, 171)',
+            borderColor: 'rgb(175, 171, 171)',
             data: [154, 250, 190, 160, 120, 130, 150]
         }]
     },
